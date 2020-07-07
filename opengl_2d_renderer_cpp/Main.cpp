cec7fccd5e9bd246cc1bc335169aadc2313e8de8
bustosfrancis97 <36048583+bustosfrancis97@users.noreply.github.com>
2020-07-06 18:21:28 -0700

    Completed window context and render context
    May need to rewrite how the window is displayed

--- opengl_2d_renderer_cpp/Main.cpp
+++ opengl_2d_renderer_cpp/Main.cpp
@@ -4,21 +4,64 @@
 #include "wglext.h"
 #pragma comment(lib, "opengl32.lib")
 
-void render() {
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
+class Window {
+public:
+	struct Config {
+		int width;
+		int height;
+		int posX;
+		int posY;
+		bool windowed;
+	} config;
+
+	LPTSTR windowClass;
+	HGLRC RC;
+	HDC DC;
+	HWND WND;
+	DWORD style;
+
+	Window();
+	~Window();
+	int create(HINSTANCE hInstance, int nCmdShow);
+	ATOM registerClass(HINSTANCE hInstance);
+	void render();
+	void swapBuffers();
+	void adjustSize();
+	void center();
+	void destroy();
+	static LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+private:
+};
+
+Window::Window() {
+	config.width = 1024;
+	config.height = 720;
+	config.posX = CW_USEDEFAULT;
+	config.posY = 0;
+	config.windowed = true;
+	style = WS_CAPTION | WS_SYSMENU | WS_CLIPSIBLINGS | WS_CLIPCHILDREN;
 }
 
-int main() {
+Window::~Window() {
+}
 
-	HWND fakeWND = CreateWindow(				// Handle of Window
-		"Core", "Fake Window",					// Window class, Title
-		WS_CLIPSIBLINGS | WS_CLIPCHILDREN,		// Style
-		0, 0,									// Window position
-		1, 1,									// Window size
-		NULL, NULL,								// Parent window, Menu
-		NULL, NULL);						// Instance, param
-	HDC fakeDC = GetDC(fakeWND);				// Device context (Handle of Context Device)
+int Window::create(HINSTANCE hInstance, int nCmdShow) {
+
+	windowClass = MAKEINTATOM(registerClass(hInstance));
+	if (windowClass == 0) {
+		MessageBox(0, "RegisterClass() failed.", "Window::create", MB_ICONERROR);
+		return 1;
+	}
+
+	// Temporary window
+	HWND fakeWND = CreateWindow(
+		windowClass, "Fake Window",		// Window, Class Title
+		style,						// Style
+		0, 0,						// X Position, Y Position
+		1, 1,						// Width, Height
+		NULL, NULL,					// Parent Window, Menu
+		hInstance, NULL);			// Instance, Param
+	HDC fakeDC = GetDC(fakeWND);	// Device Context
 
 	PIXELFORMATDESCRIPTOR fakePFD;
 	ZeroMemory(&fakePFD, sizeof(fakePFD));
@@ -32,49 +75,195 @@ int main() {
 
 	int fakePFDID = ChoosePixelFormat(fakeDC, &fakePFD);
 	if (fakePFDID == 0) {
+		MessageBox(0, "ChoosePixelFormat() failed.", "Window::create", MB_ICONERROR);
+		return 1;
+	}
+	if (SetPixelFormat(fakeDC, fakePFDID, &fakePFD) == false) {
+		MessageBox(0, "SetPixelFormat() failed.", "Window::create", MB_ICONERROR);
 		return 1;
 	}
 
-	if (SetPixelFormat(fakeDC, fakePFDID, &fakePFD) == false) {
-		return 2;
-	}
-
-	HGLRC fakeRC = wglCreateContext(fakeDC);	// Rendering context
-
+	HGLRC fakeRC = wglCreateContext(fakeDC);	// Rendering Context
 	if (fakeRC == 0) {
-		return 3;
+		MessageBox(0, "wglCreateContext() failed.", "Window::create", MB_ICONERROR);
+		return 1;
 	}
-
 	if (wglMakeCurrent(fakeDC, fakeRC) == false) {
-		return 4;
+		MessageBox(0, "wglMakeCurrent() failed.", "Window::create", MB_ICONERROR);
+		return 1;
 	}
 
 	PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatARB = nullptr;
 	wglChoosePixelFormatARB = reinterpret_cast<PFNWGLCHOOSEPIXELFORMATARBPROC>(wglGetProcAddress("wglChoosePixelFormatARB"));
 	if (wglChoosePixelFormatARB == nullptr) {
-		return 5;
+		MessageBox(0, "wglGetProcAddress() failed.", "Window::create", MB_ICONERROR);
+		return 1;
 	}
 
 	PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsARB = nullptr;
 	wglCreateContextAttribsARB = reinterpret_cast<PFNWGLCREATECONTEXTATTRIBSARBPROC>(wglGetProcAddress("wglCreateContextAttribsARB"));
 	if (wglCreateContextAttribsARB == nullptr) {
-		return 6;
+		MessageBox(0, "wglGetProcAddress() failed.", "Window::create", MB_ICONERROR);
+		return 1;
 	}
 
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-	SwapBuffers(fakeDC);
+	if (config.windowed == true) {
+		adjustSize();
+		center();
+	}
 
+	HWND WND = CreateWindow(
+		windowClass, "OpenGL Window",		// Class Name, Window Name
+		style,							// Style
+		config.posX, config.posY,		// X Position, Y Position
+		config.width, config.height,	// Width, Height
+		NULL, NULL,						// Parent Window, Menu
+		hInstance, NULL);				// Instance, Param
+
+	HDC DC = GetDC(WND);
+
+	const int pixelAttribs[] = {
+		WGL_DRAW_TO_WINDOW_ARB, GL_TRUE,
+		WGL_SUPPORT_OPENGL_ARB, GL_TRUE,
+		WGL_DOUBLE_BUFFER_ARB, GL_TRUE,
+		WGL_PIXEL_TYPE_ARB, WGL_TYPE_RGBA_ARB,
+		WGL_ACCELERATION_ARB, WGL_FULL_ACCELERATION_ARB,
+		WGL_COLOR_BITS_ARB, 32,
+		WGL_ALPHA_BITS_ARB, 8,
+		WGL_DEPTH_BITS_ARB, 24,
+		WGL_STENCIL_BITS_ARB, 8,
+		WGL_SAMPLE_BUFFERS_ARB, GL_TRUE,
+		WGL_SAMPLES_ARB, 4,
+		0 };
+
+	int pixelFormatID;
+	UINT numFormats;
+	bool status = wglChoosePixelFormatARB(DC, pixelAttribs, NULL, 1, &pixelFormatID, &numFormats);
+
+	if (status == false || numFormats == 0) {
+		MessageBox(0, "wglChoosePixelFormatARB() failed.", "Window::create", MB_ICONERROR);
+		return 1;
+	}
+
+	PIXELFORMATDESCRIPTOR PFD;
+	DescribePixelFormat(DC, pixelFormatID, sizeof(PFD), &PFD);
+	SetPixelFormat(DC, pixelFormatID, &PFD);
+
+	const int major_min = 4, minor_min = 5;
+	int contextAttribs[] = {
+		WGL_CONTEXT_MAJOR_VERSION_ARB, major_min,
+		WGL_CONTEXT_MINOR_VERSION_ARB, minor_min,
+		WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
+		0 };
+
+	HGLRC RC = wglCreateContextAttribsARB(DC, 0, contextAttribs);
+	if (RC == NULL) {
+		MessageBox(0, "wglCreateContextAttribs() failed.", "Window::create", MB_ICONERROR);
+		return 1;
+	}
+
+	// Delete temporary context and window
 	wglMakeCurrent(NULL, NULL);
 	wglDeleteContext(fakeRC);
 	ReleaseDC(fakeWND, fakeDC);
 	DestroyWindow(fakeWND);
-	if (!wglMakeCurrent(fakeDC, fakeRC)) {
-		return 7;
+	if (!wglMakeCurrent(DC, RC)) {
+		MessageBox(0, "wglMakeCurrent() failed.", "Window::create", MB_ICONERROR);
+		return 1;
 	}
 
-	SetWindowText(fakeWND, reinterpret_cast<LPCSTR>(glGetString(GL_VERSION)));
-	ShowWindow(fakeWND, 0);
+	// Initialize OpenGl loader in an extremely safe way
+	SetWindowText(WND, (LPCSTR)glGetString(GL_VERSION));
+	ShowWindow(WND, nCmdShow);
 
 	return 0;
+}
+
+ATOM Window::registerClass(HINSTANCE hInstance) {
+	WNDCLASSEX wcex;
+	ZeroMemory(&wcex, sizeof(wcex));
+	wcex.cbSize = sizeof(wcex);
+
+	// CS_HREDRAW and CS_VREDRAW will redraw the window if it is moved or resized, CS_OWNDC will get a unique Device Context for each window in the class
+	wcex.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wcex.lpfnWndProc = WindowProcedure;
+	wcex.hInstance = hInstance;
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.lpszClassName = "Core";
+
+	return RegisterClassEx(&wcex);
+}
+
+LRESULT CALLBACK Window::WindowProcedure(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	switch (message) {
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE) {
+			PostQuitMessage(0);
+		}
+		break;
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;	// Message handled
+}
+
+void Window::render() {
+	glClearColor(0.129f, 0.586f, 0.949f, 1.0f);		// R 33, G 150, B 243
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void Window::swapBuffers() {
+	SwapBuffers(DC);
+}
+
+void Window::adjustSize() {
+	RECT rect = { 0, 0, config.width, config.height };
+	AdjustWindowRect(&rect, style, false);
+	config.width = rect.right - rect.left;
+	config.height = rect.bottom - rect.top;
+}
+
+void Window::center() {
+	RECT primaryDisplaySize;
+	SystemParametersInfo(SPI_GETWORKAREA, 0, &primaryDisplaySize, 0);
+	config.posX = (primaryDisplaySize.right - config.width) / 2;
+	config.posY = (primaryDisplaySize.bottom - config.height) / 2;
+}
+
+void Window::destroy() {
+	wglMakeCurrent(NULL, NULL);
+	if (RC) {
+		wglDeleteContext(RC);
+	}
+	if (DC) {
+		ReleaseDC(WND, DC);
+	}
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+	Window window;
+
+	ReleaseDC(window.WND, window.DC);
+	
+	if (window.create(hInstance, nCmdShow) != 0) {
+		PostQuitMessage(1);
+	}
+	MSG msg;
+	bool active = true;
+	while (active) {
+		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			if (msg.message == WM_QUIT) {
+				active = false;
+			}
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		window.render();
+		window.swapBuffers();
+	}
+	window.destroy();
+	return msg.wParam;
 }
